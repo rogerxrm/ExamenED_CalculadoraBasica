2824b83051fcd1f95f261c820d3d454c0f724e57
Roger <rsalord20977@iesjoanramis.org>
2024-12-12 18:34:50 +0100

    Programa con comentarios Javadoc

--- src/calculadora/SumaNumeros.java
+++ src/calculadora/SumaNumeros.java
@@ -1,18 +1,26 @@
 package calculadora;
+//Importamos un scaner
 import java.util.Scanner;  
 public class SumaNumeros {
+	//Creamos el metodo main
 	public static void main(String[] args) {  
 		
+		//Inicializamos el scaner
 		Scanner scanner = new Scanner(System.in);  
 		
+		//Te dice que ingreses un numero y lo escanea
 		System.out.print("Ingresa el primer número: ");  
 		int numero1 = scanner.nextInt();  
 		
+		//Te dice que ingreses otro numero y lo escanea
 		System.out.print("Ingresa el segundo número: ");  
 		int numero2 = scanner.nextInt();  
 		
+		//Crea una variable que es igual a la suma de los dos numeros ingresadors
 		int suma = numero1 + numero2;  
-		System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);  
+		
+		//Te printea por pantalla el resultado de la suma
+		System.out.println(" La suma de " + numero1 + " y " + numero2 + " es: " + suma);  
 		}  
 }
 
